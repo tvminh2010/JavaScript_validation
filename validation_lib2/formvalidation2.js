f4773f3e621f3c67cbda7292fc7e765a2060d6f7
Minh Trinh Van <tvminh2010@gmail.com>
2022-01-14 16:18:28 +0700

    validation continue

--- validation_lib2/formvalidation2.js
+++ validation_lib2/formvalidation2.js
@@ -1,35 +1,52 @@
 
 function Validator (formSelector) { 
-
     /**
      * Tạo function chứa các validation rules, 
      * Có tên là validationRules
      * là 1 đối tượng chứa các rule để validate bên dưới
+     * Hướng dẫn sử dụng:
+     *  - Mỗi input phải có 1 tên khác nhau bởi thuộc tính name
+     *  - Mỗi input có 1 attribute là rules, liệt kê các rule, phân cách nhau bởi dấu '|' để validate
+     *      + required, email, min, max, min_max
+     *      + confirm_password      : 
      */
-    /* ---------------------------------------- */
     var validationRules = {
         required: function(value) {
             return value ? undefined : 'Yêu cầu nhập trường này';
         },
         email: function(value) {
             var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-            return regex.test(value) ? '' : 'Không phải địa chỉ email!';
+            return regex.test(value) ? undefined : 'Không phải địa chỉ email!';
         },
-        min: function(value) {
-            return function(min) {
-                return value.length >= min ? undefined : `Vui lòng nhập ít nhất ${min} ký tự!`;
+        min: function(min) {
+            return function(value) {
+                return value.length >= min ? undefined : `Vui lòng nhập ít nhất: '${min}' ký tự!`;
+            }  
+        },
+        max: function(max) {
+            return function(value) {
+                return value.length >= max ? undefined : `Vui lòng nhập tối đa: '${max}' ký tự!`;
+            }  
+        },
+        min_max: function(min, max) {
+            return function(value) {
+                if(value.length < min || value.length > max) {
+                    return `Số ký tự phải từ ${min} đến ${max} ký tự`;
+                }
             }
-            
+        },
+        confirm_password: function(passwordSelector) {
+            return function(value) {
+                return value === document.querySelector(passwordSelector).value  ? undefined : 'Mật khẩu ko khớp'; 
+            }
         },
     }
     /* ---------------------------------------- */
-
     formRules = {};
     //formRules[input.name] = input.getAttribute('rules');
     formElement = document.querySelector(formSelector);
     /* ---------------------------------------- */
     if (formElement) {
-
        //Nếu thấy form, lấy các thẻ inputs
        var inputs = formElement.querySelectorAll('input[name][rules]');
 
@@ -43,19 +60,25 @@ function Validator (formSelector) {
            //Lọc các rules của mỗi input vừa lấy được
            for(var rule of rules) {
 
-                var ruleFunc =  validationRules[rule];
+                var ruleFunc;
 
                 if(rule.includes(':')) {
                     ruleInfos = rule.split(':')
-                    rule = ruleInfos[0].trim();             //Nếu rule có dấu ':' lấy rule là phần tử đầu của mảng
-                    parametes = ruleInfos.splice(1)         //Lấy các tham số từ vị trí thứ 2 đến cuối mảng đưa vào parameters
-                    ruleFunc = validationRules[rule](...parametes);     //Lấy hàm bên trong của rule kèm thêm tham số vào, sử dụng closure và 
-
+                    rule = ruleInfos[0].trim();                     //Nếu rule có dấu ':' lấy rule là phần tử đầu của mảng
+                    parametes = ruleInfos.splice(1)                 //Lấy các tham số từ vị trí thứ 2 đến cuối mảng đưa vào parameters
+                    //console.log(parametes);
+                    ruleFunc = validationRules[rule](...parametes); //Lấy hàm bên trong của rule kèm thêm tham số vào, sử dụng closure và 
+                } else if (rule.includes('=')){
+                    ruleInfos = rule.split('=')
+                    rule = ruleInfos[0].trim();
+                    parametes = ruleInfos.splice(1)
+                    ruleFunc = validationRules[rule](...parametes);
                 } else {
                     rule = rule.trim();
+                    ruleFunc =  validationRules[rule];
                 }
-                //Nếu formRules[input.name] không phải mảng, là lần đầu chạy
-                // => tạo mảng và gán phần tử đầu là function có tên là rule,
+
+                //Nếu formRules[input.name] không phải mảng, => tạo mảng và gán phần tử đầu là function ruleFunc,
                 if(!Array.isArray(formRules[input.name])) {                  
                     formRules[inputname] = [ruleFunc];
                 } 
@@ -66,12 +89,11 @@ function Validator (formSelector) {
                     formRules[inputname].push(ruleFunc); 
                 }
            }    //Kết thúc lọc các rules của mỗi input
-           //Lắng nghe sự kiện validate
 
            input.onblur = handlerValidate;
-
        })//Kết thúc lọc các inputs
-       /* ------------------------------------------------- */
+       //console.log(formRules);
+    /* ------------------------------------------------- */
        function handlerValidate(event) {
             var rules = formRules[event.target.name];
             var errorMessage;
@@ -82,21 +104,39 @@ function Validator (formSelector) {
 
 
             //Nếu có lỗi thì hiển thị ra UI
+            var formGroup = getParent(event.target, '.form-group');
             if(errorMessage) {
-                var formGroup = getParent(event.target, '.form-group');
                 if(formGroup) {
                     var messageElement = formGroup.querySelector('.form-message');
                     if(messageElement) {
                         messageElement.innerText = errorMessage;
+                        this.classList.add('field-invalid')
                     }
                         
                 }
-                
+            } else {
+                if(formGroup) {
+                    var messageElement = formGroup.querySelector('.form-message');
+                    if(messageElement) {
+                        messageElement.innerText = '';
+                        this.classList.remove('field-invalid')
+                    }
+                        
+                }
             }
 
        }
+       /* ------------------------------------------------- */
+       formElement.onsubmit = function (event)  {
+            event.preventDefault()
+            var inputs = formElement.querySelectorAll('input[name][rules]');
+            inputs.forEach(input => {
+                handlerValidate({ target: input })
+            })
+        }
+        /* ------------------------------------------------- */
     } 
-    /* ------------------------------------------------- */
+
     function getParent(element, selector) {
         while (element.parentElement) {
             if(element.parentElement.matches(selector)) {
